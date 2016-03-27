8b7963c5ef601b6d0efe3d4c6d25f35bc005e353
rozie <rozie@poczta.onet.pl>
2016-03-27 09:48:33 +0200

    List of blogs (jogger URL, new URL, feed URL, name) added Helper script to generate feed list to ini file added

---
+++ make_ini.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+# script to generate feed part of planet configuration with alphabetical order
+# INPUT exactly 4 arguments, space separated. 3rd is feed URL, 4th is name
+
+use strict;
+use warnings;
+
+my %feeds=();
+
+while (<>){
+	chomp;
+	if (/(\S+)\s+(\S+)\s+(http\S+)\s+(\S+)/){
+		$feeds{$4}=$3;
+	}
+	else {
+		print "ERROR Wrong format in line: $_\n"
+	}
+}
+
+foreach my $name (sort keys %feeds){
+	print "[$feeds{$name}]\n";
+	print "name = $name\n\n"
+}
